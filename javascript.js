fefac03aa0b6973b316b6fa59b31b23d8fce53cd
Sam Rogers <sam.rogers.kiwi@gmail.com>
2023-09-26 16:10:02 +1300

    got partially working, will come back another day

--- javascript.js
+++ javascript.js
@@ -5,19 +5,23 @@ let countY = 1;
 let iterations = 0;
 let prevMousePosX;
 let prevMousePosY;
+
 squareContainer.addEventListener('mousemove', event => {
-    //square.style.transform = `rotateY(0deg) rotateX(0deg)`;
-    let mousePosX = ((event.clientX) - 364);
-    let mousePosY = ((event.clientY) - 202);
-    if (iterations > 0){
-        if(mousePosX > prevMousePosX) countX += 1.5;
-        else if (mousePosX < prevMousePosX) countX -= 1.5;
-        if(mousePosY > prevMousePosY) countY += 1.5;
-        else if (mousePosY < prevMousePosY) countY -= 1.5;
-    }
-    //square.style.transform = `rotateX(${mousePosY}deg) rotateY(${mousePosX}deg)`;
-    square.style.transform = `rotateY(${countX}deg) rotateX(${countY}deg)`;
-    iterations += 1;
-    prevMousePosX = mousePosX;
-    prevMousePosY = mousePosY;
+    if (event.buttons == 1){
+        //square.style.transform = `rotateY(0deg) rotateX(0deg)`;
+        let mousePosX = ((event.clientX) - 364);
+        let mousePosY = ((event.clientY) - 202);
+        if (iterations > 0){
+            if(mousePosX > prevMousePosX) countX -= 1.38;
+            else if (mousePosX < prevMousePosX) countX += 1.38;
+            if(mousePosY > prevMousePosY) countY -= 1.38;
+            else if (mousePosY < prevMousePosY) countY += 1.38;
+        }
+        if (countX > 360 || countX < -360) countX = 0;
+        if (countY > 360 || countY < -360) countY = 0;
+        square.style.transform = `rotateY(${countX}deg) rotateX(${countY}deg)`;
+        iterations += 1;
+        prevMousePosX = mousePosX;
+        prevMousePosY = mousePosY;
+    }   
 });
